8f350b55825956d33f242d7689a202b4c520eeb9
Julian Gilyard <juliangilyard@gmail.com>
2016-03-07 13:55:16 -0500

    Adding Randomness to the Gallery and Additional Picture Support

--- funcs.js
+++ funcs.js
@@ -3,7 +3,8 @@ function myFunction() {
                 it =1;
                 var tip = setInterval(function(){
                 var image = document.getElementById('test_gallery');
-                    it = it% 2;
+                    it = Math.floor((Math.random() * 33) + 0);
+                    it = it % 33;
                     image.src= "Photos/Test_"+it+ ".jpg";
                     it++;
                     }, 3000);
