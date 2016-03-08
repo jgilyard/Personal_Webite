1afe5da01b703fb54f76d405d208fcc9da97859e
Julian Gilyard <juliangilyard@gmail.com>
2016-03-08 03:37:31 -0500

    Added Publication Section

--- funcs.js
+++ funcs.js
@@ -8,4 +8,16 @@ function myFunction() {
                     image.src= "Photos/Test_"+it+ ".jpg";
                     it++;
                     }, 3000);
+            }     
+function myFunction_2() {
+                var it;
+                it =1;
+                var tip = setInterval(function(){
+                    it = Math.floor((Math.random() * 33) + 0);
+                    it = it % 33;
+                    var holder = "Photos/Test_"+it+ ".jpg";
+                    document.getElementById('back_change').style.background = "url("+holder + ") no-repeat center";
+                    
+                    it++;
+                    }, 3000);
             }       
